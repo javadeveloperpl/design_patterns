cdfce326ee9106156ae2c888274771b57f722554
Zbyszek <zbyszek@mail.abc>
2019-01-17 15:58:26 +0100

    Updated FormattedContext.java file

--- src/main/java/pl/javadeveloper/design/patterns/strategy/FormatterContext.java
+++ src/main/java/pl/javadeveloper/design/patterns/strategy/FormatterContext.java
@@ -2,19 +2,15 @@ package pl.javadeveloper.design.patterns.strategy;
 
 public class FormatterContext {
 
-	private TextFormatterStrategy strategy;
+    private TextFormatterStrategy strategy;
 
-	public FormatterContext() {
-		strategy = new UpperCaseFormatter();
-	}
+    public void set(TextFormatterStrategy strategy) {
+        this.strategy = strategy;
+    }
 
-	public void set(TextFormatterStrategy strategy) {
-		this.strategy = strategy;
-	}
-	
-	public void print(String text) {
-		String formattedText = strategy.format(text);
-		System.out.println(formattedText);
-	}
+    public void print(String text) {
+        String formattedText = strategy.format(text);
+        System.out.println(formattedText);
+    }
 
 }
